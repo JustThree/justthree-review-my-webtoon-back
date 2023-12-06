8b9903d569287f3a83f7420c8abd589d1a6f3195
SEONGSOO LEE <babysoo123@gmail.com>
2023-12-06 09:14:18 +0900

    refactor: userId Long으로 변경

--- src/main/java/com/java/JustThree/controller/UserController.java
+++ src/main/java/com/java/JustThree/controller/UserController.java
@@ -1,11 +1,9 @@
 package com.java.JustThree.controller;
 
-import com.java.JustThree.dto.JoinDTO;
-import com.java.JustThree.dto.UsersDTO;
+import com.java.JustThree.dto.JoinRequest;
 import com.java.JustThree.service.UsersService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +15,7 @@ public class UserController {
     private final UsersService usersService;
 
     @PostMapping(value = "/join")
-    public UsersDTO join(@RequestBody JoinDTO joinDTO){
+    public Long join(JoinRequest joinDTO){
 
         return usersService.insertUsers(joinDTO);
     }
--- src/main/java/com/java/JustThree/repository/UsersRepository.java
+++ src/main/java/com/java/JustThree/repository/UsersRepository.java
@@ -3,6 +3,8 @@ package com.java.JustThree.repository;
 import com.java.JustThree.domain.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UsersRepository extends JpaRepository<Users,Integer> {
+import java.util.Optional;
 
+public interface UsersRepository extends JpaRepository<Users,Long> {
+    public Optional<Users> findByUsersEmail(String usersEmail);
 }
