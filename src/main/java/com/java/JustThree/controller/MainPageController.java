c14e98e2040c13a420f924412bbd98dcc0df988d
tlagusejr <starc13@naver.com>
2023-12-08 13:56:21 +0900

    Feat: 단건조회 기능 추가

--- src/main/java/com/java/JustThree/controller/MainPageController.java
+++ src/main/java/com/java/JustThree/controller/MainPageController.java
@@ -1,10 +1,14 @@
 package com.java.JustThree.controller;
 
 import com.java.JustThree.domain.Webtoon;
+import com.java.JustThree.dto.main.response.WebtoonDetailResponse;
 import com.java.JustThree.service.WebtoonService;
 import lombok.AllArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.HashMap;
@@ -12,6 +16,7 @@ import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
+@RequestMapping("/api/webtoon")
 @RestController
 @AllArgsConstructor
 public class MainPageController {
@@ -19,6 +24,25 @@ public class MainPageController {
     WebtoonService webtoonService;
 
 
+    // 단건 조회
+    // 성인웹툰 거르기...
+    @GetMapping("/{id}")
+    public ResponseEntity<?> webtoonDetail(@PathVariable(name = "id") Long id){
+        try {
+            return ResponseEntity.ok()
+                    .body(webtoonService.getWebtoonDetail(id));
+        } catch (IllegalArgumentException e){
+            return ResponseEntity
+                    .notFound()
+                    .header("error",e.getMessage())
+                    .build();
+        } catch (NullPointerException e){
+            return ResponseEntity
+                    .status(404)
+                    .header("error",e.getMessage())
+                    .build();
+        }
+    }
 //    @GetMapping("/dbinit")
 //    public String init(){
 //        Map<String, Webtoon> mapJson = new HashMap<>();
--- src/main/java/com/java/JustThree/repository/WebtoonRepository.java
+++ src/main/java/com/java/JustThree/repository/WebtoonRepository.java
@@ -2,6 +2,8 @@ package com.java.JustThree.repository;
 
 import com.java.JustThree.domain.Users;
 import com.java.JustThree.domain.Webtoon;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,6 @@ import java.util.Optional;
 
 @Repository
 public interface WebtoonRepository extends JpaRepository<Webtoon,Long> {
+    Page<Webtoon> findByAgeGradCdNmIsOrderByPusryBeginDeDesc(String ageGradCdNm, Pageable pageable);
+
 }
