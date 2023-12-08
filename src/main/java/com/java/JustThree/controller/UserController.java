ade636b400f767b84b1c6933440932e41193fbdb
SEONGSOO LEE <babysoo123@gmail.com>
2023-12-08 10:01:28 +0900

    Feat: 테스트용 mapping추가

--- src/main/java/com/java/JustThree/controller/UserController.java
+++ src/main/java/com/java/JustThree/controller/UserController.java
@@ -3,6 +3,7 @@ package com.java.JustThree.controller;
 import com.java.JustThree.dto.JoinRequest;
 import com.java.JustThree.service.UsersService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,12 @@ public class UserController {
     public Long join(JoinRequest joinDTO){
 
         return usersService.insertUsers(joinDTO);
+    }
+
+    @GetMapping(value = "/authorizetest")
+    public String asdf(){
+
+        return "테스트용";
     }
 
 
