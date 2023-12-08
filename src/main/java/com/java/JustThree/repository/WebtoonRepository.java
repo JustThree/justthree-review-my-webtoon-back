3329a95aa67cb36cd46fd49a45f1e8705d5f6e9e
tlagusejr <starc13@naver.com>
2023-12-08 12:08:38 +0900

    Feat: 웹툰 리포지토리 수정

--- src/main/java/com/java/JustThree/repository/WebtoonRepository.java
+++ src/main/java/com/java/JustThree/repository/WebtoonRepository.java
@@ -5,7 +5,8 @@ import com.java.JustThree.domain.Webtoon;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WebtoonRepository extends JpaRepository<Webtoon,Long> {
-
 }
