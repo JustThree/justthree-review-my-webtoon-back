70e0656bf505a963619e449e5ffd2445fab0b939
tlagusejr <starc13@naver.com>
2023-12-12 15:24:03 +0900

    Rename: 리포지토리 파일 이동

---
+++ src/main/java/com/java/JustThree/repository/StarRepository.java
@@ -0,0 +1,22 @@
+package com.java.JustThree.repository;
+
+import com.java.JustThree.domain.Star;
+import com.java.JustThree.domain.Users;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface StarRepository extends JpaRepository<Star,Long> {
+
+
+    List<Star> findByUsers_UsersId(Long usersId);
+
+    Long countByUsers_UsersId(Long usersId);
+
+    // 웹툰에서 평가 수 모두 조회
+    List<Star> findByWebtoon_MasterIdIs(Long webtoon_masterId);
+
+    Optional<Star> findByWebtoon_MasterIdIsAndUsers_UsersIdIs(Long webtoon_masterId, Long users_usersId);
+
+}
